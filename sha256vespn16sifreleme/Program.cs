fb60b882a73efa64b752db95c5e6713b34d1c815
Alihan Budak <alihanbudak@icloud.com>
2022-07-28 17:17:45 +0300

    Proje dosyası ekle.

---
+++ sha256vespn16sifreleme/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace sha256vespn16sifreleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Şifrelenecek Metni Girin: ");
+            string sifrelenecek = Console.ReadLine();
+            SHA256Managed sha256 = new SHA256Managed();
+            byte[] bitDizisi = System.Text.Encoding.UTF8.GetBytes(sifrelenecek);
+            string sifreliVeri = Convert.ToBase64String(sha256.ComputeHash(bitDizisi));
+            Console.WriteLine("Girdiğiniz Metnin Şifreli Hali: " + sifreliVeri);
+            
+        }
+    }
+}
